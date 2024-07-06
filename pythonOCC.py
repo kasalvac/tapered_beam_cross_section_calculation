c81e0d8cbc25a3cf561dc7a5ca2e675546532406
kasalvac <kasal.emailer@gmail.com>
2024-07-06 12:55:17 +0200

    other shapes

--- pythonOCC.py
+++ pythonOCC.py
@@ -10,9 +10,10 @@ from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_MakeEdge, BRepBuilderAPI_Make
 from OCC.Core.BRepMesh import BRepMesh_IncrementalMesh
 from OCC.Core.TopExp import TopExp_Explorer
 from OCC.Core.TopAbs import TopAbs_EDGE, TopAbs_FACE
-from OCC.Core.TopoDS import topods_Face
+from OCC.Core.TopoDS import topods_Face, TopoDS_Compound, TopoDS_Iterator
 from OCC.Core.Geom import Geom_Line
 from OCC.Core.GeomAPI import GeomAPI_IntCS
+from OCC.Core.BRep import BRep_Tool
 
 # Starting the vizualization
 display, start_display, add_menu, add_function_to_menu = init_display()
@@ -44,10 +45,10 @@ def create_mesh(shape):
 
 def draw_line_origin(direction):
     # Create an Origin
-    start_pnt = gp_Pnt(0,0,0)
+    start_pnt = gp_Pnt(x_plane,0,0)
 
     # Specify the direction of the line
-    line_dir = gp_Dir(direction[0], 0, direction[1])
+    line_dir = gp_Dir(0, direction[0], direction[1])
 
     #Making the line
     line_origin = Geom_Line(start_pnt, line_dir).Lin()
@@ -60,8 +61,8 @@ def draw_line_origin(direction):
 
 # Definition the plane
 x_plane = 10
-point_on_plane = gp_Pnt(0, 0, 0)  # Point on the plane
-normal_to_plane = gp_Dir(0, 1, 0)  # Normal vector to the plane
+point_on_plane = gp_Pnt(x_plane, 0, 0)  # Point on the plane
+normal_to_plane = gp_Dir(1, 0, 0)  # Normal vector to the plane
 plane = gp_Pln(point_on_plane, normal_to_plane)
 
  # Load the STEP model
@@ -78,11 +79,49 @@ mesh = create_mesh(sliced_model)# Create a mesh from the sliced model
 direction = (1, 1)  # Direction vector
 line_origin = draw_line_origin(direction)
 
-display.DisplayShape(line_origin)
+# display.DisplayShape(line_origin, color="black")
 
 # display.DisplayShape(step_model, color="blue")
-display.DisplayShape(sliced_model, color="red")
+# display.DisplayShape(sliced_model, color="red")
 
-display.View_Iso()
+shape_iterator = TopoDS_Iterator(step_model)
+while shape_iterator.More():
+    subshape = shape_iterator.Value()
+    # Process the subshape as needed
+    print(subshape.ShapeType())  # This will print the type of the subshape
+    shape_iterator.Next()
+
+shape_iterator = TopoDS_Iterator(sliced_model)
+while shape_iterator.More():
+    subshape = shape_iterator.Value()
+    # Process the subshape as needed
+    print(subshape.ShapeType())  # This will print the type of the subshape
+    shape_iterator.Next()
+
+
+def extract_edge_curve(edge):
+    """Extracts the geometric curve from an edge."""
+    curve, _, _ = BRep_Tool.Curve(edge)  # Retrieve the geometric curve of the edge
+    return curve
+
+def extract_wire_function(wire):
+    """Extracts the functions describing the edges of a wire."""
+    edge_functions = []
+    explorer = TopExp_Explorer(wire, TopAbs_EDGE)  # Iterate over the edges of the wire
+    while explorer.More():
+        edge = explorer.Current()
+        curve = extract_edge_curve(edge)
+        edge_functions.append(curve)
+        explorer.Next()
+    return edge_functions
+
+# Assuming 'my_wire' is your TopoDS_Wire
+# Extract the functions describing the edges of the wire
+wire_functions = extract_wire_function(sliced_model)
+for idx, edge_function in enumerate(wire_functions, start=1):
+    print(f"Edge {idx} function:", edge_function)
+    
+
+# display.View_Iso()
 # display.FitAll()
 start_display()
