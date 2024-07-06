5bb8a744ae8a62350d894c398348cbde079f96c5
kasalvac <kasal.emailer@gmail.com>
2024-07-06 12:46:44 +0200

    managed to make working lines

--- pythonOCC.py
+++ pythonOCC.py
@@ -1,15 +1,21 @@
-# Working visualization of the slice
+# managed to make working lines
 
+import math
 from OCC.Display.SimpleGui import init_display
 from OCC.Core.TopoDS import TopoDS_Shape
 from OCC.Core.STEPControl import STEPControl_Reader
 from OCC.Core.BRepAlgoAPI import BRepAlgoAPI_Section
-from OCC.Core.gp import gp_Pnt, gp_Dir, gp_Pln
+from OCC.Core.gp import gp_Pnt, gp_Dir, gp_Pln, gp_Vec
 from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_MakeEdge, BRepBuilderAPI_MakeWire
 from OCC.Core.BRepMesh import BRepMesh_IncrementalMesh
 from OCC.Core.TopExp import TopExp_Explorer
 from OCC.Core.TopAbs import TopAbs_EDGE, TopAbs_FACE
 from OCC.Core.TopoDS import topods_Face
+from OCC.Core.Geom import Geom_Line
+from OCC.Core.GeomAPI import GeomAPI_IntCS
+
+# Starting the vizualization
+display, start_display, add_menu, add_function_to_menu = init_display()
 
 def load_step_model(file_path):
     reader = STEPControl_Reader()
@@ -36,29 +42,47 @@ def create_mesh(shape):
     mesh.Perform()
     return mesh
 
+def draw_line_origin(direction):
+    # Create an Origin
+    start_pnt = gp_Pnt(0,0,0)
+
+    # Specify the direction of the line
+    line_dir = gp_Dir(direction[0], 0, direction[1])
+
+    #Making the line
+    line_origin = Geom_Line(start_pnt, line_dir).Lin()
+    line_origin = BRepBuilderAPI_MakeEdge(line_origin)
+    line_origin.Build()
+    line_origin = line_origin.Shape()
+    return line_origin
+
+
+
 # Definition the plane
-point_on_plane = gp_Pnt(1000, 0, 0)  # Point on the plane
-normal_to_plane = gp_Dir(1, 0, 0)  # Normal vector to the plane
+x_plane = 10
+point_on_plane = gp_Pnt(0, 0, 0)  # Point on the plane
+normal_to_plane = gp_Dir(0, 1, 0)  # Normal vector to the plane
 plane = gp_Pln(point_on_plane, normal_to_plane)
 
-# Load the STEP model
-step_model_path = "Rechteckrohr_verjuengt_Volumen.stp"
+ # Load the STEP model
+step_model_path = "Fluegelhuelle_Test3.stp"
 step_model = load_step_model(step_model_path)
 
-# Slice the model with the plane
-sliced_model = slice_model_with_plane(step_model, plane)
+# Slicing of the model and extracting the wire model
+sliced_model = slice_model_with_plane(step_model, plane)# Slice the model with the plane
+faces = extract_faces(sliced_model)# Extract faces from the sliced model
+mesh = create_mesh(sliced_model)# Create a mesh from the sliced model
 
-# Extract faces from the sliced model
-faces = extract_faces(sliced_model)
 
-# Create a mesh from the sliced model
-mesh = create_mesh(sliced_model)
+# Making the points and lines that will intersect the shape
+direction = (1, 1)  # Direction vector
+line_origin = draw_line_origin(direction)
 
-# Visualize the result
-display, start_display, add_menu, add_function_to_menu = init_display()
+display.DisplayShape(line_origin)
 
-display.DisplayShape(step_model, color="blue")
+# display.DisplayShape(step_model, color="blue")
 display.DisplayShape(sliced_model, color="red")
 
-display.FitAll()
+display.View_Iso()
+# display.FitAll()
 start_display()
