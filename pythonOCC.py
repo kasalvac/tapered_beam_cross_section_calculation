aff5c8bfba39cba99aa1e58f83d54ad6eb7235a2
kasalvac <kasal.emailer@gmail.com>
2024-07-06 11:44:46 +0200

    load file

--- pythonOCC.py
+++ pythonOCC.py
@@ -1,1 +1,9 @@
-print('hello world')
+from OCC.Core.GProp import GProp_GProps
+from OCC.Core.BRepGProp import brepgprop_VolumeProperties
+from OCC.Extend.DataExchange import read_step_file
+
+my_shape = read_step_file('Rechteckrohr_verjuengt_Volumen.stp')
+prop = GProp_GProps()
+tolerance = 1e-5 # Adjust to your liking
+volume = brepgprop_VolumeProperties(my_shape, prop, tolerance)
+print(volume)
