6384a213f15f052fbd12cfd1ef7891fcc444c201
kasalvac <kasal.emailer@gmail.com>
2024-07-09 10:34:21 +0200

    cleaned up the code and deleted the unnecesary functions

--- pythonOCC_1.5.py
+++ pythonOCC_1.5.py
@@ -2,21 +2,13 @@
 
 import math
 from OCC.Display.SimpleGui import init_display
-from OCC.Core.TopoDS import TopoDS_Shape
 from OCC.Core.STEPControl import STEPControl_Reader
 from OCC.Core.BRepAlgoAPI import BRepAlgoAPI_Section
-from OCC.Core.gp import gp_Pnt, gp_Dir, gp_Pln, gp_Vec
-from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_MakeEdge, BRepBuilderAPI_MakeWire
-from OCC.Core.BRepMesh import BRepMesh_IncrementalMesh
-from OCC.Core.TopExp import TopExp_Explorer
-from OCC.Core.TopAbs import TopAbs_EDGE, TopAbs_FACE
-from OCC.Core.TopoDS import topods_Face
+from OCC.Core.gp import gp_Pnt, gp_Dir, gp_Pln
+from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_MakeEdge
 from OCC.Core.Geom import Geom_Line
-from OCC.Core.GeomAPI import GeomAPI_IntCS
-
 from OCC.Core.BRepExtrema import BRepExtrema_DistShapeShape
-from OCC.Core.gp import gp_Pnt
-from OCC.Core.TopoDS import TopoDS_Shape
+
 
 # Starting the vizualization
 display, start_display, add_menu, add_function_to_menu = init_display()
@@ -31,20 +23,6 @@ def slice_model_with_plane(model, plane):
     section_algo = BRepAlgoAPI_Section(model, plane)
     section_algo.Build()
     return section_algo.Shape()
-
-def extract_faces(shape):
-    face_explorer = TopExp_Explorer(shape, TopAbs_FACE)
-    faces = []
-    while face_explorer.More():
-        face = topods_Face(face_explorer.Current())
-        faces.append(face)
-        face_explorer.Next()
-    return faces
-
-def create_mesh(shape):
-    mesh = BRepMesh_IncrementalMesh(shape, 0.1)  # Adjust the deflection parameter if trouble :D 
-    mesh.Perform()
-    return mesh
 
 def draw_line_origin(direction):
     # Create an Origin
@@ -61,7 +39,6 @@ def draw_line_origin(direction):
     return line_origin
 
 
-
 # Definition the plane
 x_plane = 10
 point_on_plane = gp_Pnt(x_plane, 0, 0)  # Point on the plane
@@ -72,25 +49,17 @@ plane = gp_Pln(point_on_plane, normal_to_plane)
 step_model_path = "Rechteckrohr_verjuengt_Volumen.stp"
 step_model = load_step_model(step_model_path)
 
-# Slicing of the model and extracting the wire model
+# Slicing of the model - extracting the wire model
 sliced_model = slice_model_with_plane(step_model, plane)# Slice the model with the plane
-faces = extract_faces(sliced_model)# Extract faces from the sliced model
-mesh = create_mesh(sliced_model)# Create a mesh from the sliced model
-
 
 # Making the points and lines that will intersect the shape
-direction = (1, 0)  # Direction vector
+direction = (1, 1)  # Direction vector
 line_origin = draw_line_origin(direction)
 
 # Finding the intersections
-# Create an instance of BRepExtrema_DistShapeShape
 extrema_calculator = BRepExtrema_DistShapeShape(sliced_model, line_origin)
-
-# Perform the calculation to find intersections
-extrema_calculator.Perform()
-
-# Get the number of intersections
-num_intersections = extrema_calculator.NbSolution()
+extrema_calculator.Perform() # Perform the calculation to find intersections
+num_intersections = extrema_calculator.NbSolution() # Get the number of intersections
 
 # Iterate through intersections and get the points
 intersection_points = []
