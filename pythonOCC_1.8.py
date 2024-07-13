ca8450b2a83bfc99311da678278d715926db9d50
kasalvac <kasal.emailer@gmail.com>
2024-07-13 23:34:48 +0200

    calculating area succesfully

--- pythonOCC_1.8.py
+++ pythonOCC_1.8.py
@@ -1,4 +1,4 @@
-# working bizilion lines and find bizilion inntersections
+# calculating area succesfully
 
 import math
 from OCC.Display.SimpleGui import init_display
@@ -11,7 +11,7 @@ from OCC.Core.BRepExtrema import BRepExtrema_DistShapeShape
 
 
 # Preparing the vizualization
-display, start_display, add_menu, add_function_to_menu = init_display()
+# display, start_display, add_menu, add_function_to_menu = init_display()
 
 def load_step_model(file_path):
     reader = STEPControl_Reader()
@@ -26,14 +26,16 @@ def slice_model_with_plane(model, plane):
 
 def draw_line_origin(direction):
     # Create an Origin
-    start_pnt = gp_Pnt(0,0,0)
+    start_pnt = gp_Pnt(x_plane,0,0)
 
     # Specify the direction of the line
-    line_dir = gp_Dir(direction[0], 0, direction[1])
+    line_dir = gp_Dir(0, direction[0], direction[1])
 
     #Making the line
     line_origin = Geom_Line(start_pnt, line_dir).Lin()
-    line_origin = BRepBuilderAPI_MakeEdge(line_origin)
+    negative_limiter = gp_Pnt(0, - direction[0], - direction[1])
+    positive_limiter = gp_Pnt(0, 9999 * direction[0], 9999 * direction[1]) # needs sizing based on the size of the wire!
+    line_origin = BRepBuilderAPI_MakeEdge(negative_limiter, positive_limiter)
     line_origin.Build()
     line_origin = line_origin.Shape()
     return line_origin
@@ -43,8 +45,8 @@ def find_intersections_in(sliced_model, line_origin): # Find the points on the i
     extrema_calculator = BRepExtrema_DistShapeShape(sliced_model, line_origin)
     extrema_calculator.Perform() # Perform the calculation to find intersections
     num_intersections = extrema_calculator.NbSolution() # Get the number of intersections
-    if num_intersections != 4:
-        return "Error: num_intersections != 4"
+    if num_intersections != 2:
+        return "Error: num_intersections != 2"
 
     # Iterate through intersections and get the points
     points_on_plane = []
@@ -52,8 +54,10 @@ def find_intersections_in(sliced_model, line_origin): # Find the points on the i
         extrema_point = extrema_calculator.PointOnShape1(i)
         intersection_point = gp_Pnt(extrema_point.X(), extrema_point.Y(), extrema_point.Z())
         points_on_plane.append(intersection_point)
-    intersection_points_in.append(points_on_plane[0])# But only save the first two points since they are the innner points
-    intersection_points_in.append( points_on_plane[1])
+    if (math.sqrt(points_on_plane[0].Y() ** 2 + points_on_plane[0].Z() ** 2) < math.sqrt(points_on_plane[1].Y() ** 2 + points_on_plane[1].Z() ** 2)): #input the check of distances! And save acordingly smaller value
+        intersection_points_in.append(points_on_plane[0])
+    else:
+        intersection_points_in.append(points_on_plane[1])
     return intersection_points_in
 
 def find_intersections_out(sliced_model, line_origin): # Find the points on the outer shell
@@ -61,8 +65,8 @@ def find_intersections_out(sliced_model, line_origin): # Find the points on the 
     extrema_calculator = BRepExtrema_DistShapeShape(sliced_model, line_origin)
     extrema_calculator.Perform() # Perform the calculation to find intersections
     num_intersections = extrema_calculator.NbSolution() # Get the number of intersections
-    if num_intersections != 4:
-        return "Error: num_intersections != 4"
+    if num_intersections != 2:
+        return "Error: num_intersections != 2"
 
     # Iterate through intersections and get the points
     points_on_plane = []
@@ -70,20 +74,22 @@ def find_intersections_out(sliced_model, line_origin): # Find the points on the 
         extrema_point = extrema_calculator.PointOnShape1(i)
         intersection_point = gp_Pnt(extrema_point.X(), extrema_point.Y(), extrema_point.Z())
         points_on_plane.append(intersection_point)
-    intersection_points_out.append(points_on_plane[2]) # But only save the last two points since they are the outer points
-    intersection_points_out.append(points_on_plane[3])
+    if (math.sqrt(points_on_plane[0].Y() ** 2 + points_on_plane[0].Z() ** 2) > math.sqrt(points_on_plane[1].Y() ** 2 + points_on_plane[1].Z() ** 2)): #input the check of distances! And save acordingly bigger value
+        intersection_points_out.append(points_on_plane[0])
+    else:
+        intersection_points_out.append(points_on_plane[1])
     return intersection_points_out
 
 
 
 # Definition the plane
-x_plane = 10
-point_on_plane = gp_Pnt(0, 0, 0)  # Point on the plane
-normal_to_plane = gp_Dir(0, 1, 0)  # Normal vector to the plane
+x_plane = 1000
+point_on_plane = gp_Pnt(x_plane, 0, 0)  # Point on the plane
+normal_to_plane = gp_Dir(1, 0, 0)  # Normal vector to the plane
 plane = gp_Pln(point_on_plane, normal_to_plane)
 
 # Load the STEP model
-step_model_path = "Fluegelhuelle_Test3.stp"
+step_model_path = "Rechteckrohr_verjuengt_Volumen.stp"
 step_model = load_step_model(step_model_path)
 
 # Slicing of the model - extracting the wire model
@@ -92,40 +98,42 @@ sliced_model = slice_model_with_plane(step_model, plane)# Slice the model with t
 
 intersection_points_in = [] # Declare empty list for inner shell
 intersection_points_out = [] # Declare empty list for outer shell
-desired_num_points = 100
-for i in range(1, desired_num_points + 1):
-    angle_pich = math.pi / desired_num_points
+desired_num_points = 1000
+for i in range(0, desired_num_points):
+    angle_pich = (2 * math.pi) / desired_num_points
     cur_angle = i * angle_pich
     direction =  (math.sin(cur_angle), math.cos(cur_angle))
     line_origin = draw_line_origin(direction)
     intersection_points_in = find_intersections_in(sliced_model, line_origin)
     intersection_points_out = find_intersections_out(sliced_model, line_origin)
 
-# print('inner shell')
-# print(intersection_points_in)
-# print('outer shell')
-# print(intersection_points_out)
-    
-for point in intersection_points_in:
-    display.DisplayShape(point, color="blue")
+area = 0
+for i in range (0, len(intersection_points_in) - 1): # Calculating areas of triangles using vertexes - formula half of sum of things #googleit 
+    tri_area_1 = (1/2)*abs(((intersection_points_in[i].Y()*(intersection_points_in[i+1].Z() - (intersection_points_out[i].Z()))) + (intersection_points_in[i+1].Y()*((intersection_points_out[i].Z()) - intersection_points_in[i].Z())) + (intersection_points_out[i].Y()*(intersection_points_in[i].Z() - intersection_points_in[i+1].Z()))))
+    area += tri_area_1
+    tri_area_2 = (1/2)*abs(((intersection_points_in[i+1].Y()*(intersection_points_out[i+1].Z() - (intersection_points_out[i].Z()))) + (intersection_points_out[i+1].Y()*((intersection_points_out[i].Z()) - intersection_points_in[i+1].Z())) + (intersection_points_out[i].Y()*(intersection_points_in[i+1].Z() - intersection_points_out[i+1].Z()))))
+    area += tri_area_2
 
-for point in intersection_points_out:
-    display.DisplayShape(point, color="green")
+print('area:')
+print(area)
 
-# Making the points and lines that will intersect the shape
-# direction = (1, 0)  # Direction vector
-# line_origin = draw_line_origin(direction)
+# for i in range (0, 10):
+#     print('coordinates:')
+#     print(intersection_points_in[i].Y(), intersection_points_in[i].Z())
+#     print(intersection_points_out[i].Y(), intersection_points_out[i].Z())
 
-# intersection_points = find_intersections(sliced_model, line_origin)
 
-# x = intersection_points[0].X()
-# print(f"X coordinate: {x}")
+# for point in intersection_points_in:
+#     display.DisplayShape(point, color="black")
+
+# for point in intersection_points_out:
+#     display.DisplayShape(point, color="green")
 
 
 # Vizualization
 # display.DisplayShape(line_origin)
 # display.DisplayShape(step_model, color="blue")
-display.DisplayShape(sliced_model, color="red")
+# display.DisplayShape(sliced_model, color="red")
 # display.View_Iso()
-display.FitAll()
-start_display()
+# display.FitAll()
+# start_display()
