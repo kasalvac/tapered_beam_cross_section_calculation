d3b642eb5bf8bfe111ad804df195eedca25f7c80
kasalvac <kasal.emailer@gmail.com>
2024-08-13 09:39:39 +0200

    making it calculate along the whole wing box

--- pythonOCC_2.1.py
+++ pythonOCC_2.1.py
@@ -20,142 +20,77 @@ import pandas as pd
 # display, start_display, add_menu, add_function_to_menu = init_display()
 
 def load_step_model(file_path):
-    """
-    Loads a STEP file and returns the geometric shape.
-
-    Parameters:
-    file_path (str): The path to the STEP file to be loaded.
-
-    Returns:
-    TopoDS_Shape: The shape object extracted from the STEP file.
-    """
     reader = STEPControl_Reader()
     reader.ReadFile(file_path)
     reader.TransferRoots()
     return reader.Shape()
 
 def slice_model_plane_par_x(model, x_coordinate):
-    """
-    Slice a 3D model with a plane parallel to the YZ-plane at a given x-coordinate.
-
-    Parameters:
-    model : The 3D model to be sliced.
-    x_coordinate : float
-        The x-coordinate at which the slicing plane is defined.
-
-    Returns:
-    section_shape : The cross-section shape resulting from the slicing operation.
-    """
-    # Define the plane
-    point_on_plane = gp_Pnt(x_coordinate, 0, 0)  # Point on the plane, also the center of the cross-section
+    # Definition the plane
+    point_on_plane = gp_Pnt(x_coordinate, 0, 0)  # Point on the plane als othe center of the cross section
     normal_to_plane = gp_Dir(1, 0, 0)  # Normal vector to the plane
     plane = gp_Pln(point_on_plane, normal_to_plane)
-
-    # Slice the model with the defined plane
+    # slicing the model with the defined plane
     section_algo = BRepAlgoAPI_Section(model, plane)
     section_algo.Build()
-
-    # Return the resulting cross-section shape
     return section_algo.Shape()
 
 def draw_line_origin(direction):
-    """
-    Creates a line starting from the origin (0, 0, 0) extending in the specified direction.
-    
-    Parameters:
-    direction (list or tuple): A list or tuple with two elements specifying the Y and Z 
-                               direction components of the line.
-    
-    Returns:
-    TopoDS_Shape: The shape of the line created.
-    """
-    # Create the starting point at the origin
-    start_pnt = gp_Pnt(0, 0, 0)
-    
+    # Create an Origin
+    start_pnt = gp_Pnt(x_plane,0,0)
+
     # Specify the direction of the line
     line_dir = gp_Dir(0, direction[0], direction[1])
-    
-    # Create the line using the start point and direction
-    geom_line = Geom_Line(start_pnt, line_dir).Lin()
-    
-    # Define the points that limit the line (from a negative to a positive extreme)
-    negative_limiter = gp_Pnt(0, -direction[0], -direction[1])
-    positive_limiter = gp_Pnt(0, 9999 * direction[0], 9999 * direction[1])  # Needs sizing based on the size of the wire
-    
-    # Make an edge from the limiting points
-    line_edge = BRepBuilderAPI_MakeEdge(negative_limiter, positive_limiter)
-    line_edge.Build()
-    
-    # Get the shape of the line
-    line_shape = line_edge.Shape()
-    
-    return line_shape
 
-def find_intersections_in(sliced_model, line_origin):
-    """
-    Find the intersection points between a sliced model and a line.
+    #Making the line
+    line_origin = Geom_Line(start_pnt, line_dir).Lin()
+    negative_limiter = gp_Pnt(0, - direction[0], - direction[1])
+    positive_limiter = gp_Pnt(0, 9999 * direction[0], 9999 * direction[1]) # needs sizing based on the size of the wire!
+    line_origin = BRepBuilderAPI_MakeEdge(negative_limiter, positive_limiter)
+    line_origin.Build()
+    line_origin = line_origin.Shape()
+    return line_origin
 
-    Parameters:
-    sliced_model (TopoDS_Shape): The sliced model to check for intersections.
-    line_origin (TopoDS_Shape): The line used to find intersections with the sliced model.
-
-    Returns:
-    list: A list of intersection points on the inner shell.
-    """
-    # Initialize the intersection calculator with the sliced model and the line
+def find_intersections_in(sliced_model, line_origin): # Find the points on the inner shell
+    # Finding the intersections
     extrema_calculator = BRepExtrema_DistShapeShape(sliced_model, line_origin)
-    extrema_calculator.Perform()  # Perform the calculation to find intersections
-    num_intersections = extrema_calculator.NbSolution()  # Get the number of intersections
+    extrema_calculator.Perform() # Perform the calculation to find intersections
+    num_intersections = extrema_calculator.NbSolution() # Get the number of intersections
+    # if num_intersections != 2:
+    #     return "Error: num_intersections != 2"
 
-    # List to store the intersection points
+    # Iterate through intersections and get the points
     points_on_plane = []
-
-    # Iterate through the intersections and get the points
     for i in range(1, num_intersections + 1):
         extrema_point = extrema_calculator.PointOnShape1(i)
         intersection_point = gp_Pnt(extrema_point.X(), extrema_point.Y(), extrema_point.Z())
         points_on_plane.append(intersection_point)
 
-    # Determine the closer intersection point based on Y and Z coordinates
-    if math.sqrt(points_on_plane[0].Y() ** 2 + points_on_plane[0].Z() ** 2) < math.sqrt(points_on_plane[1].Y() ** 2 + points_on_plane[1].Z() ** 2):
+    if (math.sqrt(points_on_plane[0].Y() ** 2 + points_on_plane[0].Z() ** 2) < math.sqrt(points_on_plane[1].Y() ** 2 + points_on_plane[1].Z() ** 2)): #input the check of distances! And save acordingly smaller value
         intersection_points_in.append(points_on_plane[0])
     else:
         intersection_points_in.append(points_on_plane[1])
-
     return intersection_points_in
 
-def find_intersections_out(sliced_model, line_origin):
-    """
-    Find the intersection points on the outer shell of a given model.
-
-    Parameters:
-    - sliced_model: The model to be intersected, represented as a TopoDS_Shape.
-    - line_origin: The reference line or point used for finding intersections, represented as a TopoDS_Shape.
-
-    Returns:
-    - intersection_points_out: A list containing the intersection point on the outer shell of the model.
-    """
-
-    # Perform the intersection calculation
+def find_intersections_out(sliced_model, line_origin): # Find the points on the outer shell
+    # Finding the intersections
     extrema_calculator = BRepExtrema_DistShapeShape(sliced_model, line_origin)
-    extrema_calculator.Perform()  # Perform the calculation to find intersections
-    num_intersections = extrema_calculator.NbSolution()  # Get the number of intersections
+    extrema_calculator.Perform() # Perform the calculation to find intersections
+    num_intersections = extrema_calculator.NbSolution() # Get the number of intersections
+    # if num_intersections != 2:
+    #     return "Error: num_intersections != 2"
 
-    # Initialize a list to store points on the plane
+    # Iterate through intersections and get the points
     points_on_plane = []
-    
-    # Iterate through intersections and collect the points
     for i in range(1, num_intersections + 1):
         extrema_point = extrema_calculator.PointOnShape1(i)
         intersection_point = gp_Pnt(extrema_point.X(), extrema_point.Y(), extrema_point.Z())
         points_on_plane.append(intersection_point)
-
-    # Determine the point that lies further from the origin in the YZ plane
+        
     if (math.sqrt(points_on_plane[0].Y() ** 2 + points_on_plane[0].Z() ** 2) > math.sqrt(points_on_plane[1].Y() ** 2 + points_on_plane[1].Z() ** 2)): #input the check of distances! And save acordingly bigger value
         intersection_points_out.append(points_on_plane[0])
     else:
         intersection_points_out.append(points_on_plane[1])
-
     return intersection_points_out
 
 def calculate_triangle_area(p1, p2, p3):
@@ -191,7 +126,7 @@ def calculate_triangle_area(p1, p2, p3):
     
     return area
 
-def calculate_triangle_moment_of_inertia_Y(p1, p2, p3):
+def calculate_moment_of_inertia_Y(p1, p2, p3):
     """
     Calculate the moment of inertia of a triangle defined by three points about a specified axis.
 
@@ -231,7 +166,7 @@ def calculate_triangle_moment_of_inertia_Y(p1, p2, p3):
 
     return Ys
 
-def calculate_triangle_moment_of_inertia_Z(p1, p2, p3):
+def calculate_moment_of_inertia_Z(p1, p2, p3):
     """
     Calculate the moment of inertia of a triangle defined by three points about a specified axis.
 
@@ -272,13 +207,19 @@ def calculate_triangle_moment_of_inertia_Z(p1, p2, p3):
     return Ys
 
 # Open the *.txt-file with coordinated where to colaculate
-sensor_info = pd.read_csv('Sensor_Positions_Vaclav_fixed.txt', delimiter=',', header=None)
-sensor_info.columns = ['ID', 'X', 'Y', 'Z']
+sensor_info = pd.read_csv('Sensor_Positions3.txt', delimiter=',', header=None)
+sensor_info.columns = ['ID', 'Y', 'X', 'Z']
 sensor_info = sensor_info.astype({"ID": int, "X": float, "Y": float, "Z": float})
 
 # Load the STEP model
-step_model_path = "Tapered-Beam-Volume.stp"
+step_model_path = "Rechteckrohr_verjuengt_Volumen.stp"
 step_model = load_step_model(step_model_path)
+
+# x_plane = 10
+
+# sensor_info['Area'] = 0.0
+# sensor_info['centery'] = 0.0
+# sensor_info['centerz'] = 0.0
 
 sensor_info['distance'] = 0.0
 sensor_info['inertiaZ'] = 0.0
@@ -286,7 +227,7 @@ sensor_info['inertiaY'] = 0.0
 
 # Loop through each value in column with X values
 for index, value in sensor_info['X'].items():
-    # Perform all the calculations calculation needs to be more pretty in the future
+    # Perform all the calculations calculation needs to be mor epretty in the future
 
     x_plane = value
 
@@ -333,26 +274,29 @@ for index, value in sensor_info['X'].items():
         cent_mass_Z_prepare += tri_area_1 * tri_centr_Y_1 + tri_area_2 * tri_centr_Z_2
 
         # Using the funtion to calculate the moment of inertia for the first triangle
-        moment_of_inertia_Y += calculate_triangle_moment_of_inertia_Y(intersection_points_in[i], intersection_points_in[i+1], intersection_points_out[i])
+        moment_of_inertia_Y += calculate_moment_of_inertia_Y(intersection_points_in[i], intersection_points_in[i+1], intersection_points_out[i])
 
         # Using the funtion to calculate the moment of inertia for the second triangle
-        moment_of_inertia_Y += calculate_triangle_moment_of_inertia_Y(intersection_points_in[i+1], intersection_points_out[i], intersection_points_out[i+1])
+        moment_of_inertia_Y += calculate_moment_of_inertia_Y(intersection_points_in[i+1], intersection_points_out[i], intersection_points_out[i+1])
 
         # Using the funtion to calculate the moment of inertia for the first triangle
-        moment_of_inertia_Z += calculate_triangle_moment_of_inertia_Z(intersection_points_in[i], intersection_points_in[i+1], intersection_points_out[i])
+        moment_of_inertia_Z += calculate_moment_of_inertia_Z(intersection_points_in[i], intersection_points_in[i+1], intersection_points_out[i])
 
         # Using the funtion to calculate the moment of inertia for the second triangle
-        moment_of_inertia_Z += calculate_triangle_moment_of_inertia_Z(intersection_points_in[i+1], intersection_points_out[i], intersection_points_out[i+1])
+        moment_of_inertia_Z += calculate_moment_of_inertia_Z(intersection_points_in[i+1], intersection_points_out[i], intersection_points_out[i+1])
 
 
 
-    # finalizing the calculation of center of mass has to be done outside the loop because total area is requiered
+    # finalizing the calculation of center of mass
     cent_mass_Y = cent_mass_Y_prepare / area
     cent_mass_Z = cent_mass_Z_prepare / area
 
-    # finalizing the calculation of distance to neutral fiber
     distance_neutral_fiber = math.sqrt((cent_mass_Y - sensor_info.at[index, 'Y'])**2 + (cent_mass_Z - sensor_info.at[index, 'Z'])**2)
 
+    # Add the calculated value to the corecponding column
+    # sensor_info.at[index, 'Area'] = area
+    # sensor_info.at[index, 'centery'] = cent_mass_Y
+    # sensor_info.at[index, 'centerz'] = cent_mass_Z
 
     sensor_info.at[index, 'distance'] = distance_neutral_fiber
     sensor_info.at[index, 'inertiaZ'] = moment_of_inertia_Z
@@ -361,3 +305,22 @@ for index, value in sensor_info['X'].items():
 
 print('sensor info:')
 print(sensor_info)
+
+# print('area:')
+# print(area)
+
+# print('center of mass:')
+# print(f"X: {cent_mass_Y}, Y: {cent_mass_Z}")
+
+# print('moment_of_inertia_Z:')
+# print(moment_of_inertia_Z)
+# print('moment_of_inertia_Y:')
+# print(moment_of_inertia_Y)
+
+# Vizualization
+# display.DisplayShape(line_origin)
+# display.DisplayShape(step_model, color="blue")
+# display.DisplayShape(sliced_model, color="red")
+# display.View_Iso()
+# display.FitAll()
+# start_display()
